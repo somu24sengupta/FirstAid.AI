0bf69d630e72c670110b85770d47575edbabe721
Soumya Sengupta <senguptasoumya24@gmail.com>
2025-04-07 12:23:39 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'FirstAid.AI',
+    version= '0.0.0',
+    author= 'Soumya Sengupta',
+    author_email= 'senguptasoumya24@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
